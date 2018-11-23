cfc89bd4efa84d9e54a6360e243208f656da8370
WChmura <wchmura28@gmail.com>
2018-11-23 17:57:43 +0100

    Initial commit

---
+++ src/FrontEnd/Page.java
@@ -0,0 +1,9 @@
+package FrontEnd;
+import java.applet.*;
+import java.awt.*;
+
+public class Page extends Applet {
+    public void paint (Graphics g) {
+        g.drawString ("Hello World", 25, 50);
+    }
+}
---
+++ src/FrontEnd/Views/Marks.java
@@ -0,0 +1,6 @@
+package FrontEnd.Views;
+
+import FrontEnd.Page;
+
+public class Marks extends Page {
+}
