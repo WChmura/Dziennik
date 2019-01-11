98938563908b01f2398d954ea60da7126ec9467d
WChmura <wchmura28@gmail.com>
2019-01-11 20:28:25 +0100

    PersonalData view

--- src/FrontEnd/Views/PersonalData.java
+++ src/FrontEnd/Views/PersonalData.java
@@ -1,11 +1,16 @@
 package FrontEnd.Views;
 
+import Common.MockModel;
+import Common.UserType;
 import FrontEnd.Page;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PersonalData extends Page {
 
-    public String error;
+    private boolean canEdit;
     //To potrzebuje
     private String firstName;
     private String secondName;
@@ -37,34 +42,114 @@ public class PersonalData extends Page {
 
     @Override
     public void createGUI() {
-        // co sprawia, że tutaj mogę ich użyć:
-        this.addTopMenu(4);
-        if(error==null){
-            this.addSubPanel( firstNamePanel(),50);
-            this.addSubPanel( secondNamePanel(),50);
-            this.addSubPanel( adressPanel(),50);
-            // przy okazji, jakbyś chciał wywołać funkcje np. showAttendance() - do podejrzenia z diagramu Models na draw.io
-            // to robisz to tak:
-            // model.showAttendance(studentID);
-            // a model StudentPanel załaduje dane, wywoła funkcję createGUI widoku Attendance z zapełnionymi polami
-            // i tak się to toczy
-        }
-        // new Text(error);
+        this.addTopMenu(7);
+        if(MockModel.getUserType()!= UserType.student)
+            canEdit = true;
+        else
+            canEdit = false;
+        this.addSubPanel( firstNamePanel(),50);
+        this.addSubPanel( secondNamePanel(),50);
+        this.addSubPanel( adressPanel(),50);
+        this.addSubPanel( genderPanel(),50);
+        this.addSubPanel( personalNumberPanel(),50);
+        if(canEdit)
+            this.addSubPanel(buttonPanel(),50);
     }
+
     private JPanel firstNamePanel(){
-        JPanel score = new JPanel();
-        score.add(new JLabel(firstName));
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JTextField textField = new JTextField(firstName);
+        if(!canEdit)
+            textField.setEnabled(false);
+        JLabel label = new JLabel("Imie:");
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        score.add(label);
+        score.add(textField);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
         return score;
     }
+
     private JPanel secondNamePanel(){
-        JPanel score = new JPanel();
-        score.add(new JLabel(secondName));
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JTextField textField = new JTextField(firstName);
+        if(!canEdit)
+            textField.setEnabled(false);
+        JLabel label = new JLabel("Nazwisko:");
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        score.add(label);
+        score.add(textField);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
         return score;
     }
+
     private JPanel adressPanel(){
-        JPanel score = new JPanel();
-        score.add(new JLabel(adress));
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JTextField textField = new JTextField(firstName);
+        if(!canEdit)
+            textField.setEnabled(false);
+        JLabel label = new JLabel("Adres:");
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        score.add(label);
+        score.add(textField);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
         return score;
+    }
+
+    private JPanel genderPanel(){
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JTextField textField = new JTextField(firstName);
+        if(!canEdit)
+            textField.setEnabled(false);
+        JLabel label = new JLabel("Płeć:");
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        score.add(label);
+        score.add(textField);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        return score;
+    }
+
+    private JPanel personalNumberPanel(){
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JTextField textField = new JTextField(firstName);
+        if(!canEdit)
+            textField.setEnabled(false);
+        JLabel label = new JLabel("PESEL:");
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        score.add(label);
+        score.add(textField);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        return score;
+    }
+
+    private JPanel buttonPanel(){
+        JPanel score = new JPanel(new GridLayout(1,5,10,10));
+        JButton button = new JButton("Zapisz zmiany");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: wyslij zmiany
+            }
+        });
+        score.add(emptyLabel());
+        score.add(button);
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        score.add(emptyLabel());
+        return score;
+    }
+
+    private JLabel emptyLabel(){
+        return new JLabel("");
     }
 
 }
--- src/FrontEnd/Views/Schedule.java
+++ src/FrontEnd/Views/Schedule.java
@@ -9,16 +9,17 @@ import java.awt.*;
 public class Schedule extends Page {
     //to potrzebuje
     private String subjects[][];//format np "nazwaPrzedmiotu:sala", w tabeli dzien godzina
-    private String hours[];// godziny rozpoczecia zajec
     private String groupName; //nazwa klasy
     private String allGroupsNames[]; //nazwy wszystkich klas - tylko dla adminów
+    private int maxNumOfLesson; //maksymalna ilosc godzin w dniu
     //+ metodki
     // edycja - tylko dla adminów
     // taka c
     @Override
     public void createGUI() {
+        maxNumOfLesson =7;
         addTopMenu(1);
-        JButton button = new JButton("Schedule");
-        mainContent.add(button, BorderLayout.NORTH);
+
     }
+
 }
